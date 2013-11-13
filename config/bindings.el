4c0483bc9d44db4fd256c2c4e722d66c85741e58
Wes Morgan <cap10morgan@gmail.com>
2013-11-13 10:03:12 -0500

    add comment-or-uncomment-region-or-line function on s-/

--- config/bindings.el
+++ config/bindings.el
@@ -1,1 +1,13 @@
 (define-key global-map (kbd "RET") 'comment-indent-new-line)
+
+(defun comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if there's no active
+   region. From http://stackoverflow.com/questions/9688748/emacs-comment-uncomment-current-line"
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+        (setq beg (region-beginning) end (region-end))
+        (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
+
+(define-key global-map (kbd "s-/") 'comment-or-uncomment-region-or-line)
