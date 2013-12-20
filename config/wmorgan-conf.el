8c66d8857bcccef53a7344fe00b9f15bab46abea
Wes Morgan <cap10morgan@gmail.com>
2013-12-20 11:57:20 -0500

    make frame slightly translucent for no good reason

--- config/wmorgan-conf.el
+++ config/wmorgan-conf.el
@@ -6,3 +6,7 @@
 
 ;; start Emacs server
 (server-start)
+
+;; make window slightly translucent
+(set-frame-parameter (selected-frame) 'alpha '(95 95))
+(add-to-list 'default-frame-alist '(alpha 95 95))
